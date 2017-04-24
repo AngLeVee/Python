ff83dfc7c254348862c33b3ccb6e596a6931a1ac
Angela LeVee <alevee95@gmail.com>
2017-04-23 18:41:52 -0600

    Delete Fibonacci.py

--- Fibonacci.py
+++
@@ -1,8 +0,0 @@
-print("enter numbers")
-fibonacci = []
-fibonacci.append(0)
-fibonacci.append(1)
-for i in range (1,100):
-    fibonacci.append(fibonacci[i-1] + fibonacci[i])
-print(fibonacci)
-print("Press enter to exit" + input())
