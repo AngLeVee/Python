65f78d5d31be6c786e07399c5647e02388466983
Angela LeVee <alevee95@gmail.com>
2017-04-23 18:42:24 -0600

    Program to find first 100 fibonacci numbers

---
+++ Fibonacci.py
@@ -0,0 +1,9 @@
+print("enter numbers")
+fibonacci = []
+fibonacci.append(0)
+fibonacci.append(1)
+for i in range (1,100):
+    fibonacci.append(fibonacci[i-1] + fibonacci[i])
+print(fibonacci)
+print("Press enter to exit")
+input()
